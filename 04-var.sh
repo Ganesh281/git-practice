265ef3cfc1dc14389303400fd224b461240268e6
Ganesh281 <varagantiganesh69@gmail.com>
2024-08-27 18:11:03 +0530

    added vars USER PASS

--- 04-var.sh
+++ 04-var.sh
@@ -4,5 +4,5 @@ echo "Please enter your username"
 read USERNAME
 echo "Username enter is: $USERNAME"
 echo "Please enter password"
-read-s PASSWORD
+read -s PASSWORD
 echo "passwoord entered is: $PASSWORD"
