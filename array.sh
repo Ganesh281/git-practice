bea5d40d662419145a3e3693782c574249d978d9
Ganesh281 <varagantiganesh69@gmail.com>
2024-08-27 18:21:06 +0530

    added fruits

--- array.sh
+++ array.sh
@@ -3,7 +3,7 @@
 FRUITS=("APPLE" "KIWI" "BANANA" "CRP") #ARRAY
 
 echo "FIRST fruit is: ${FRUITS[0]}"
-echo "SECOND fruit is: ${FRUITS[0]}"
-echo "THIRD fruit is: ${FRUITS[0]}"
+echo "SECOND fruit is: ${FRUITS[1]}"
+echo "THIRD fruit is: ${FRUITS[2]}"
 
 echo "all FRIUTS IS :${FRUITS[@]}"
