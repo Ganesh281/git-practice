69e35e452f09bf1106c5fa93042b00c57179613d
Ganesh281 <varagantiganesh69@gmail.com>
2024-08-28 12:42:52 +0530

    special var

--- specialvar.sh
+++ specialvar.sh
@@ -3,3 +3,5 @@
 echo "All varaibales passed to the script: $@"
 echo "Number of variables passed: $#"
 echo "script name: $0"
+echo "current working directory: $PWD"
+echo "Home direvtory of current user: $HOME"
