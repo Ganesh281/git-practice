3f402907f187a57433174bc315fd0d1ce0781598
Ganesh281 <varagantiganesh69@gmail.com>
2024-09-01 16:04:34 +0530

    colorfunc

---
+++ colors.sh
@@ -0,0 +1,48 @@
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo"Please run this script with root privileges"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is...$R failed $N"
+        exit 1
+    else
+        echo -e "$2 is...$G success $N"
+    fi
+}
+
+
+CHECK_ROOT
+
+dnf list installed git
+
+if [$? -ne 0 ]
+then 
+    echo "git is not installed, going to install.."
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else 
+    echo "Git is already installed, noting to do.."    
+fi
+
+if [$? -ne 0 ]
+then 
+     echo "mysql is not installed, going to install.."
+     dnf install mysql -y
+     VALIDATE $? "Installing MySQL"
+     
+else
+    echo "mysql is already installed, nothing to do.."
+fi    
